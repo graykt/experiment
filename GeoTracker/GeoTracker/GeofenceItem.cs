7aa80a12d25a5139f8902f87920ebea653ebe46e
Сергей Давыдов <davydov@mega-mozg.ru>
2018-01-05 14:21:11 +0900

    GeoTracker Start

---
+++ GeoTracker/GeoTracker/GeofenceItem.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Windows.Devices.Geolocation;
+using Windows.Devices.Geolocation.Geofencing;
+
+namespace GeoTracker
+{
+    // GeofenceItem implements IEquatable to allow
+    // removal of objects in the collection
+    public class GeofenceItem : IEquatable<GeofenceItem>
+    {
+        private Geofence _geofence;
+        private string _id;
+
+        public GeofenceItem(Geofence geofence)
+        {
+            this._geofence = geofence;
+            this._id = geofence.Id;
+        }
+
+        public bool Equals(GeofenceItem other)
+        {
+            bool isEqual = false;
+            if (Id == other.Id)
+            {
+                isEqual = true;
+            }
+
+            return isEqual;
+        }
+
+        public Geofence Geofence
+        {
+            get
+            {
+                return _geofence;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public double Latitude
+        {
+            get
+            {
+                Geocircle circle = _geofence.Geoshape as Geocircle;
+                return circle.Center.Latitude;
+            }
+        }
+
+        public double Longitude
+        {
+            get
+            {
+                Geocircle circle = _geofence.Geoshape as Geocircle;
+                return circle.Center.Longitude;
+            }
+        }
+
+        public double Radius
+        {
+            get
+            {
+                Geocircle circle = _geofence.Geoshape as Geocircle;
+                return circle.Radius;
+            }
+        }
+
+        public bool SingleUse
+        {
+            get
+            {
+                return _geofence.SingleUse;
+            }
+        }
+
+        public MonitoredGeofenceStates MonitoredStates
+        {
+            get
+            {
+                return _geofence.MonitoredStates;
+            }
+        }
+
+        public TimeSpan DwellTime
+        {
+            get
+            {
+                return _geofence.DwellTime;
+            }
+        }
+
+        public DateTimeOffset StartTime
+        {
+            get
+            {
+                return _geofence.StartTime;
+            }
+        }
+
+        public TimeSpan Duration
+        {
+            get
+            {
+                return _geofence.Duration;
+            }
+        }
+    }
+}
---
+++ GeoTracker/GeoTracker/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.Devices.Geolocation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace GeoTracker
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void StartTracking(object sender, RoutedEventArgs e)
+        {
+            // Request permission to access location
+            var accessStatus = await Geolocator.RequestAccessAsync();
+
+        }
+
+        private void StopTracking(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
